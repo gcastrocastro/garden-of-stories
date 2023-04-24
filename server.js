cdcd962c871ec2de8ed10ee73ee128b83db321e2
gcastrocastro <gcastrocastro.25@gmail.com>
2023-04-24 13:38:09 -0400

    add review routes, dotenv config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const indexRoutes = require('./routes/index');
 const bookRoutes = require('./routes/books');
+const reviewRoutes = require('./routes/reviews');
 
 //initialize express application
 const app = express();
@@ -9,12 +10,18 @@ const app = express();
 // configure application settings
 app.set('view engine', 'ejs');
 
+//expose environment variables
+require('dotenv').config();
+//require and execute database config code-- has to be placed after dotenv config 
+require('./config/database'); 
+
 // mount middlware
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false })); 
 app.use(express.static('public'));
 
 app.use('/', indexRoutes);
+app.use('/', reviewRoutes);
 app.use('/books', bookRoutes);
 
 app.listen(3000, () => {
