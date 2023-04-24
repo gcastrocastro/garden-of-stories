b9bca0704e1e1b257ad567ae912bf476af59cc98
gcastrocastro <gcastrocastro.25@gmail.com>
2023-04-23 21:40:37 -0400

    add new book routes

--- routes/books.js
+++ routes/books.js
@@ -5,4 +5,7 @@ const booksController = require('../controllers/books');
 // GET /books --> index route
 router.get('/', booksController.index);
 
+//GET /new -->  new book view route
+router.get('/new', booksController.new);
+
 module.exports = router;
