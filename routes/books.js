ca3b05bb924ff232c28ecec0730993cc19d7b2e0
gcastrocastro <gcastrocastro.25@gmail.com>
2023-04-24 14:24:11 -0400

    add show route and controller

--- routes/books.js
+++ routes/books.js
@@ -5,10 +5,13 @@ const booksController = require('../controllers/books');
 // GET /books --> index route
 router.get('/', booksController.index);
 
-//GET /new -->  new book view route
+//GET /new -->  create new book view
 router.get('/new', booksController.new);
 
 //POST /books  --> create new book
 router.post('/', booksController.create);
+
+//GET /:id --> book show view
+router.get('/:id', booksController.show);
 
 module.exports = router;
