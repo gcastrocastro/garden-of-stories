6e1ffdc3e2d65919b48f7111e8d82923f7695964
gcastrocastro <gcastrocastro.25@gmail.com>
2023-04-25 19:37:09 -0400

    complete oauth cycle

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/books');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // GET /books --> index route
 router.get('/', booksController.index);
 
 //GET /new -->  create new book view
-router.get('/new', booksController.new);
+router.get('/new', ensureLoggedIn, booksController.new);
 
 //POST /books  --> create new book
-router.post('/', booksController.create);
+router.post('/', ensureLoggedIn, booksController.create);
 
 //GET /:id --> book show view
 router.get('/:id', booksController.show);
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reviewsController = require('../controllers/reviews');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // POST new review
-router.post('/books/:id/reviews', reviewsController.create);
+router.post('/books/:id/reviews', ensureLoggedIn, reviewsController.create);
 
 // DELETE review
-router.delete('/books/:id/reviews/:id', reviewsController.delete);
+router.delete('/books/:id/reviews/:id', ensureLoggedIn, reviewsController.delete);
 
 module.exports = router;
