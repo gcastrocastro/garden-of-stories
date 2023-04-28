f2ef4a7ff4a40c39ed0e7e91f95f569f63bbfc9d
gcastrocastro <gcastrocastro.25@gmail.com>
2023-04-28 17:00:45 -0400

    dlted cmmts

--- models/user.js
+++ models/user.js
@@ -14,29 +14,4 @@ const userSchema = new Schema({
     timestamps: true
   });
 
-// const userSchema = new Schema ({
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     favoriteGenre: [{
-//         type: String,
-//         enum: ['Adventure/Fantasy', 'Fiction', 'Nonfiction', 'Young Adult', 'Personal Development', 'Mystery/Thriller', 'Biography/Memoir', 'Children and Toddlers']
-//     }],
-//     ageRange: {
-//         type: String,
-//         enum: ['0-10', '11-18', '18+']
-//     }
-// })
-
 module.exports = mongoose.model('User', userSchema);
